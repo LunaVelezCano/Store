95f79da951468bf1cd3c569295ba756d9e9062b0
LunaVelezCano <luna.velezc13@gmail.com>
2024-09-26 15:03:52 -0500

    first commit

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+})
+export class CounterComponent {
+  @Input({ required: true }) duration = 0;
+  @Input({ required: true }) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor() {
+    // no async
+    //before render
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration'];
+    if(duration &&  duration.currentValue!== duration.previousValue){
+      this.doSomeThing();
+    }
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    this.counterRef = window.setInterval(()=>{
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1);
+    }, 1000)
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfetrViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef)
+  }
+
+  doSomeThing(){
+    console.log('change duration')
+  }
+}
